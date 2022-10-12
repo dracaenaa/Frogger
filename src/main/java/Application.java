3f9b69ff48a0a4d6986f550ba65bbff177e86429
Miranda Kolker <mkolker@bgsu.edu>
2022-10-12 15:21:13 -0400

    code cleanup

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -11,28 +11,20 @@ public class Application {
     UI userInterface = new UI();
     boolean isRunning = true;
     FroggoInfo froggoInfo = null;
+    StreetCrossing streetCrossing = null;
 
     private void run() {
 
-        userInterface.opening();
-        froggoInfo = new FroggoInfo(userInterface.receiveNameInput(), userInterface.receiveFavoriteColorInput(),
-                userInterface.receiveFavoriteNumberInput(), userInterface.receiveFavoriteFoodInput());
-
-        StreetCrossing streetCrossing = new StreetCrossing(froggoInfo.getFavoriteNumber());
-        userInterface.waitObservation();
+        handleIntroOutputAndInput();
 
         //run while loop until froggo dies
         while (isRunning) {
 
             if (streetCrossing.crosswalkGoing()) {
-                handleCrosswalkOn();;
+                handleCrosswalkOn();
             }
             else {
-                if (userInterface.willCross(froggoInfo)) {
-                    isRunning = streetCrossing.crossStreet();
-                } else {
-                    userInterface.waitObservation();
-                }
+                handleIfCrossOrNot();
             }
             if (streetCrossing.getStreetWidth() == streetCrossing.getLanesCrossed()) {
                 userInterface.crossToOtherSideOfRoad(froggoInfo);
@@ -53,5 +45,22 @@ public class Application {
         else {
             userInterface.crosswalkWait(froggoInfo);
         }
+    }
+
+    private void handleIfCrossOrNot() {
+        if (userInterface.willCross(froggoInfo)) {
+            isRunning = streetCrossing.crossStreet();
+        } else {
+            userInterface.waitObservation();
+        }
+    }
+
+    private void handleIntroOutputAndInput() {
+        userInterface.opening();
+        froggoInfo = new FroggoInfo(userInterface.receiveNameInput(), userInterface.receiveFavoriteColorInput(),
+                userInterface.receiveFavoriteNumberInput(), userInterface.receiveFavoriteFoodInput());
+
+        streetCrossing = new StreetCrossing(froggoInfo.getFavoriteNumber());
+        userInterface.waitObservation();
     }
 }
